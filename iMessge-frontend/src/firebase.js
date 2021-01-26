71c6362a8aac7226e8b7bf38dd17a182cf7effe9
Dinakar Pathakota <55839773+ASH04DP@users.noreply.github.com>
2021-01-26 14:44:03 +0530

    Add files via upload

---
+++ iMessge-frontend/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  // The config lines in firebase webapp option
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
